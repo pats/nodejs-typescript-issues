a4509e9427f9a3d7c0250af2a065c8d1aa35d393
morriq <dawid.winiarczyk@pracuj.pl>
2023-02-08 17:22:36 +0100

    test

--- src/throw-async.ts
+++ src/throw-async.ts
@@ -1,30 +1,25 @@
 async function thrower() {
-    throw new Error('test');
+  throw new Error('test');
 }
 
 async function level1() {
-    return await thrower();
+  return await thrower();
 }
 
 async function level2() {
-    return await level1();
+  return await level1();
 }
 
 async function level3() {
-    return await level2();
+  return await level2();
 }
 
 async function main() {
-    try {
-        await level3();
-    } catch(err) {
-        console.warn('main error', err);
-    }
+  try {
+    await level3();
+  } catch (err) {
+    console.warn('main error', err);
+  }
 }
 
-main().then(() => {
-    console.log('finished');
-}).catch((err) => {
-    console.log(...err);
-    console.error('Something went wrong', err);
-})
+export default main;
