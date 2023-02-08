a3cbb20daafb44dfaad52f5cfdc212abe197db01
morriq <dawid.winiarczyk@pracuj.pl>
2023-02-08 17:26:21 +0100

    test

--- src/throw-async.ts
+++ src/throw-async.ts
@@ -1,25 +1,10 @@
-async function thrower() {
-  throw new Error('test');
-}
+import main from 'morriq-nodejs-typescript-issues';
 
-async function level1() {
-  return await thrower();
-}
-
-async function level2() {
-  return await level1();
-}
-
-async function level3() {
-  return await level2();
-}
-
-async function main() {
-  try {
-    await level3();
-  } catch (err) {
-    console.warn('main error', err);
-  }
-}
-
-export default main;
+main()
+  .then(() => {
+    console.log('finished');
+  })
+  .catch((err) => {
+    console.log(...err);
+    console.error('Something went wrong', err);
+  });
